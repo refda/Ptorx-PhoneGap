ef387b0fbd4ac63ac0b39a1efb87ea59b3bcd948
refda <s@merq.de>
2017-12-27 21:42:44 +0100

    Update app.js

--- www/app.js
+++ www/app.js
@@ -1,8 +1,6 @@
 function onDeviceReady() {
   window.location.href =
-    'https://ptorx.com/app/?r=source~phonegap-' +
-    device.platform.split(' ')[0].toLowerCase() +
-    '&phonegap=1';
+    'https://www.traveluce.com/ios?app=1';
 }
 
 document.addEventListener('deviceready', onDeviceReady, false);
