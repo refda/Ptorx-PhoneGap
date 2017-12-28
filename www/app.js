533a15647f2c2d467283726da69cd9040bec6c03
refda <s@merq.de>
2017-12-28 18:11:49 +0100

    Update app.js

--- www/app.js
+++ www/app.js
@@ -1,6 +1,12 @@
 function onDeviceReady() {
-  window.location.href =
-    'https://www.traveluce.com/ios?app=1';
+    
+  if((navigator.connection.type).toUpperCase() != "NONE" &&
+       (navigator.network.connection.type).toUpperCase() != "UNKNOWN") {
+    window.location.href = 'https://www.traveluce.com/ios?app=1';
+    } else {
+    window.location.href="offline.html";
+    }         
+  
 }
 
 document.addEventListener('deviceready', onDeviceReady, false);
